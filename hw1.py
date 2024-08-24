72c6d768e767d1ce405fe4d2a8d85231cd2489e3
YUMO9743 <118965984+YUMO9743@users.noreply.github.com>
2024-08-24 21:23:52 +0000

    try to fix file

--- hw1.py
+++ hw1.py
@@ -4,7 +4,7 @@ def multiply_numbers(a, b):
 result = multiply_numbers(4, 1)
 print("The product is:", result)
 
-print("The result is:", reslt)
+print("The result is:", result)
 
 for i in range(5):
     print(i)

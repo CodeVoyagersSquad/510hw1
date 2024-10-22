337436dc2240bb202b21086ce902e8f036b1eea5
Komachi Zhou <32158594+KomachiZ@users.noreply.github.com>
2024-10-22 15:35:57 -0400

    Update task1.sh

--- hw4/task1.sh
+++ hw4/task1.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-pkill -f infinite.sh 
+pid=$(ps -ef | grep 'infinite.sh' | grep -v grep | awk '{print $2}')
+if [ -z "$pid" ]; then
+  echo "No process found."
+else
+  kill -9 $pid
+  echo "Process infinite.sh with PID $pid killed."
+fi
